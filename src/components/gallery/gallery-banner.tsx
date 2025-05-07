54c6a3168a843eb1e346750385f62d938d1362f9
kledyu <1017yu@daum.net>
2025-05-07 04:07:18 +0000

    style: className 수정

--- src/components/gallery/gallery-banner.tsx
+++ src/components/gallery/gallery-banner.tsx
@@ -1,8 +1,15 @@
-
 export default function GalleryBanner() {
   return (
-  <div className="w-[inherit] h-[380px] bg-[url('/images/banners/gallery-banner.jpg')] bg-center bg-cover bg-no-repeat relative flex justify-center items-center">
-    <span className="absolute text-white text-[80px] font-bold leading-none" style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.4)', fontFamily: 'Noto Sans KR' }}>ART+THERAPY 展</span>
-  </div>
+    <div
+      className='w-full h-[380px] bg-center bg-cover bg-no-repeat relative flex justify-center items-center'
+      style={{ backgroundImage: "url('/images/banners/gallery-banner.webp')" }}>
+      <span
+        className='absolute text-white text-[80px] font-bold leading-none'
+        style={{
+          textShadow: '0px 4px 4px rgba(0, 0, 0, 0.4)',
+        }}>
+        ART+THERAPY 展
+      </span>
+    </div>
   );
 }
